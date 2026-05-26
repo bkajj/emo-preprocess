f586a125dfa0dd49620b73c113edc5be764c4315
bkajj <muuciek.new@gmail.com>
2026-05-26 02:06:34 +0200

    feat: change setup to YAML file, implement dataset config and cache

--- config.py
+++ config.py
@@ -3,9 +3,12 @@ from joblib import Memory
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 EXTRACTED_PATH = os.path.join(BASE_DIR, 'extracted')
+CONFIG_PATH = os.path.join(BASE_DIR, 'config')
 
-# Features are always cached
-features_memory = Memory(location=os.path.join(BASE_DIR, '.cache/features/'), verbose=0)
+if os.environ.get('USE_FEATURE_MEMORY') == '1':
+    features_memory = Memory(location=os.path.join(BASE_DIR, '.cache/features/'), verbose=0)
+else:
+    features_memory = Memory(location=None, verbose=0)
 
 if os.environ.get('USE_DATASET_MEMORY') == '1':
     raw_dataset_memory = Memory(location=os.path.join(BASE_DIR, '.cache/raw_signal_data/'), verbose=5)
---
+++ pipeline.py
@@ -0,0 +1,75 @@
+import os 
+import yaml
+import pandas as pd 
+import argparse
+from emo_datasets import *
+import warnings;
+from config import *
+from concurrent.futures import ProcessPoolExecutor
+from benchmark import benchmark_datasets
+warnings.filterwarnings('ignore')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', '--samples', type=int, default=None)
+parser.add_argument('-c', '--config', default=os.path.join(CONFIG_PATH, 'sample.yaml'))
+args = parser.parse_args()
+
+datasets = {
+    'biraffe': Biraffe,
+    'case': Case,
+    'deap': Deap
+}
+
+def run_dataset(name, window_time, sample_size=None, thread_num=0, max_threads=1):
+    dataset = datasets[name]()
+    return dataset.run(sample_size, thread_num, max_threads, window_time)
+
+if __name__ == '__main__':
+    with open('configs/sample.yaml') as f:
+        config = yaml.safe_load(f)
+        ds_configs = config['datasets']
+
+    os.environ['USE_DATASET_MEMORY'] = '1' if config['cache']['raw_signals'] else '0'
+    os.environ['USE_FEATURE_MEMORY'] = '1' if config['cache']['features'] else '0'
+
+    jobs = []
+    for name, cfg in ds_configs.items(): # create list of jobs like (name, thread_num, max_threads)
+        if cfg['enabled']:
+            for thread_num in range(cfg['threads']):
+                jobs.append((name, thread_num, cfg['threads']))
+
+    with ProcessPoolExecutor(max_workers=len(jobs)) as executor:
+        futures = {
+            executor.submit(
+                run_dataset, 
+                name,
+                ds_configs[name]['window_time'],
+                args.samples, 
+                thread_num, 
+                max_threads
+            ): (name, thread_num) for name, thread_num, max_threads in jobs
+        }
+
+    results = {name: [] for name, cfg in ds_configs.items() if cfg['enabled']}
+    errors = {name: [] for name, cfg in ds_configs.items() if cfg['enabled']}
+
+    for future in futures:
+        name, thread_num = futures[future]
+        r, e = future.result()
+        results[name].append(r)
+        errors[name].append(e)
+
+    for name in results:
+        results[name] = pd.concat(results[name], ignore_index=True)
+        errors[name] = pd.concat(errors[name], ignore_index=True)
+
+    if args.samples is None:
+        for name in results:
+            datasets[name]().merge_subjects_to_csv()
+
+    benchmark_results = benchmark_datasets(results)
+    print(benchmark_results)
+    benchmark_results.to_csv(os.path.join(EXTRACTED_PATH, 'results.csv'), index=False)
+
+    errors_merged = pd.concat(errors.values(), ignore_index=True)
+    errors_merged.to_csv(os.path.join(EXTRACTED_PATH, 'errors.csv'), index=False)
